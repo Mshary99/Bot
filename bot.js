f939e512c67cd0e93ff260552f779792d4357a77
Mshary99 <85209404+Mshary99@users.noreply.github.com>
2021-07-03 04:51:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
-    token: process.env.TOKEN,
+    token: process.env. ODYwNjc2Njk2MzIzMjYwNDI2.YN-tow.wW0TzMfGQ-Ks8ZFwmhzm72FDQmE,
     mongoPath: process.env.mongoPath,
-    prefix: "$",
+    prefix: "€",
     expressURL: "" // if you have a express server but it here to uptime you project
 };
